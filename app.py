d1433afa525bae8a14792823031ba90cfbe264ac
Durga Chintalapudi <105465968+DurgaChintalapudi@users.noreply.github.com>
2022-09-18 03:03:20 +0000

    Minor changes to APIs

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-from fastapi import FastAPI
+'''Webapp'''
 import uvicorn
+from fastapi import FastAPI
 from dblib.connector import querydb
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
+    '''Root'''
     return {"message": "Hello Databricks"}
-
 
 @app.get("/add/{num1}/{num2}")
 async def add(num1: int, num2: int):
     """Add two numbers together"""
-
     total = num1 + num2
     return {"total": total}
 
@@ -21,9 +20,7 @@ async def add(num1: int, num2: int):
 @app.get("/query")
 async def query():
     """Execute a SQL query"""
-
     result = querydb("SELECT MAX(WITHDRAWAL_AMT) FROM default.transaction group by ACCOUNT_NO order by MAX(WITHDRAWAL_AMT) desc limit 1")
-    
     return {"result": result}
 
 
