0ed8244ac29b7a474b86d00da170115d3d5b8cef
Durga Chintalapudi <105465968+DurgaChintalapudi@users.noreply.github.com>
2022-10-02 00:10:42 +0000

    transforming the data

--- scripts/transform.sh
+++ scripts/transform.sh
@@ -2,9 +2,9 @@
 
 #load the file covid_19_data.csv from ../data to variable csvfile
 csvfile=../data/covid_19_data.csv
-
+./logs.sh info "Transforming the data"
 #convert the case to upper case in csv file
 csvfile=$(echo $csvfile | tr '[:lower:]' '[:upper:]')
-
 #remove column one from csvfile
 cut -d, -f1 --complement $csvfile > ../data/covid_19_data_clean.csv
+./logs.sh info "Moving the data to data folder: ../data/covid_19_data_clean.csv"
